ea60c8329e9ac0e04e6d74fc1d7fdb5d5c895789
Andrey Demakov <coremotor@outlook.com>
2022-01-27 13:25:50 +0300

    add mailgun

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -1,37 +1,23 @@
 import { Request, Response } from "express";
-import nodemailer from "nodemailer";
+import mailgun from "mailgun-js";
 
 export const createOrder = async (req: Request, res: Response) => {
   const { name, email, description } = req.body;
+  const mg = mailgun({
+    apiKey: process.env.MAILGUN_APIKEY,
+    domain: process.env.MAILGUN_DOMAIN,
+  });
+  const data = {
+    from: "Mailgun Sandbox <postmaster@sandboxb8ec835e31e74ece9a63b0561e1be31d.mailgun.org>",
+    to: "coremotor@outlook.com",
+    subject: "Get a consultation",
+    text: `Name: ${name} \n Email: ${email} \n Description: ${description}`,
+  };
   try {
-    const transport = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      port: 587,
-      secure: false, // upgrade later with STARTTLS
-      auth: {
-        user: process.env.SMTP_LOGIN,
-        pass: process.env.SMTP_PASSWORD,
-      },
-    });
-
-    const mailOptions = {
-      from: email,
-      to: "coremotor@gmail.com",
-      subject: `Order from ${name} ${email}`,
-      text: description,
-    };
-
-    transport.sendMail(mailOptions, function (error, response) {
-      if (error) {
-        res
-          .status(500)
-          .json({ message: "Send mail error in transport", error });
-      } else {
-        res.status(200).send("Mail is send");
-      }
-    });
+    await mg.messages().send(data);
+    res.status(200).send("Mail is send");
   } catch (e) {
     console.log(e);
-    res.status(500).send("Send mail error");
+    res.status(500).json({ message: "Send mail error", e });
   }
 };
