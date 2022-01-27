043e14a5ce46747543415bb2e3d3949be41dde36
Andrey Demakov <coremotor@outlook.com>
2022-01-27 14:38:11 +0300

    add mailgun

--- src/controllers/order.controller.ts
+++ src/controllers/order.controller.ts
@@ -2,21 +2,20 @@ import { Request, Response } from "express";
 import mailgun from "mailgun-js";
 
 export const createOrder = async (req: Request, res: Response) => {
-  const { name, email, description } = req.body;
-  const key = process.env.MAILGUN_APIKEY;
-  const domain = process.env.MAILGUN_DOMAIN;
-  console.log(key, domain);
-  const mg = mailgun({
-    apiKey: key,
-    domain: domain,
-  });
-  const data = {
-    from: "Mailgun Sandbox <postmaster@sandboxb8ec835e31e74ece9a63b0561e1be31d.mailgun.org>",
-    to: "coremotor@outlook.com",
-    subject: "Get a consultation",
-    text: `Name: ${name} \n Email: ${email} \n Description: ${description}`,
-  };
   try {
+    const { name, email, description } = req.body;
+    const apiKey = process.env.MAILGUN_APIKEY;
+    const domain = process.env.MAILGUN_DOMAIN;
+    const mg = mailgun({
+      apiKey,
+      domain,
+    });
+    const data = {
+      from: "Mailgun Sandbox <postmaster@sandboxb8ec835e31e74ece9a63b0561e1be31d.mailgun.org>",
+      to: "coremotor@outlook.com",
+      subject: "Get a consultation",
+      text: `Name: ${name} \n Email: ${email} \n Description: ${description}`,
+    };
     const response = await mg.messages().send(data);
     res.status(200).json({ message: "Mail is send", response });
   } catch (e) {
